a3a24d8fce86dc08e16f74328a26d3c854ac87b9
Alex Parella <aaparella@ucdavis.edu>
2014-06-30 20:12:03 -0700

    All new Makefile!

--- src/get.h
+++ src/get.h
@@ -9,10 +9,5 @@
 	
 	int get(MessageStruct* message, char* url); // Send get HTTP request to specified URL
 	int store(MessageStruct message);           // Store message contents to temp file
-	
-	// Function for parsing response from request
-	static size_t write_data(void *response,
-													size_t size, size_t nmemb,
-													MessageStruct *message);
-	
+		
 #endif
