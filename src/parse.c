7df0e7b69deeb87baedb9cb00d47b15933e14341
Alex Parella <aaparella@ucdavis.edu>
2014-06-30 19:46:38 -0700

    Comments and link libxml2

--- src/parse.c
+++ src/parse.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <libxml/xmlreader.h>
 #include "parse.h"
 
 
--- src/prompter.c
+++ src/prompter.c
@@ -1,3 +1,11 @@
+/************************************+
+ * Prompter.c                        *
+ * Author : Alexander Parella        *
+ * CLI RSS feed reader, source       *
+ * available at github.com/aaparella *
+ * Usage : prompter -help            *
+ +************************************/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -8,14 +16,18 @@
  
 int main(int argc, char* argv[]) {
 	
+	// Parse command line arguments, use defaults when needed
 	ArgumentStruct* args;
 	args = parseArguments(argc, argv);
 
+	// Struct to store response from CURL
 	MessageStruct response;
 	
+	// Get RSS feed from server
 	if(get(&response, args->url))
 		printf("Error in performing CURL");
-		
+	
+	// Write out result to file if needed		
 	if (!args->stdout) {
 		if(store(response))
 			printf("Error writing response to file");
@@ -23,6 +35,7 @@ int main(int argc, char* argv[]) {
 	else 
 		printf("%s", response.contents);
 	
+	// Args is dynamically allocated, free it!
 	free(args);
 	return 0;
 }
