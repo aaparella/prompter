4de5e5238dea421401198d6b98c27cc434efe244
Alex Parella <aaparella@ucdavis.edu>
2014-07-28 21:30:08 -0700

    Begin HTML parsing

--- src/htmlparse.c
+++ src/htmlparse.c
@@ -2,10 +2,34 @@
 #include <string.h>
 
 #include "htmlparse.h"
+#include "stack.h"
 
 void ParseHtml(char* html) {
     
+    Stack* stack = getNewStack();
+    printf("PARSING : %s\n", html);
     
+    char* test = strtok(html, "<");
+    printf("TEST : %s\n", test);
     
+    if (test) {
+        char* properties = strtok(NULL, ">");
+        
+        while(properties) {
+            
+            // Needs fleshing out
+            if (properties[strlen(properties) - 1] != '/')
+                push(stack, properties);
+            
+            test = strtok(NULL, "<");
+            if (!properties)
+                break;
+            
+            properties = strtok(NULL, ">");
+        }
+    }
+    
+    while((test = popElement(stack)))
+        printf("%s\n", test);
     
 }
